1156e9a08d7166ea5eabeeb2f4bb00b74c7df1ce
Semyon Shilovskiy <semen.shilovski@yandex.ru>
2023-09-13 18:22:13 +0300

    upd

---
+++ src/main/java/com/github/semen_shilovski/Hello.java
@@ -0,0 +1,11 @@
+package com.github.semen_shilovski;
+
+public class Hello {
+  
+  public static String GREETING = "com.github.semen_shilovski.Hello world!";
+  
+  public static void main(String []args) {
+    
+  }  
+}
+
